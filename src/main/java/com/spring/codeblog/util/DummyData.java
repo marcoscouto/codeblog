b3ec3fdd287c9429f0ba6ddb58defd14c3cf9b27
Marcos Couto <marcos_otuoc@hotmail.com>
2020-01-25 17:05:36 -0300

    Modifing the template posts.html

--- src/main/java/com/spring/codeblog/util/DummyData.java
+++ src/main/java/com/spring/codeblog/util/DummyData.java
@@ -5,9 +5,7 @@ import com.spring.codeblog.repository.CodeblogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
