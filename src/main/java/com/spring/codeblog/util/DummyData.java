c2e201d744cdca2cea1df6c2499b64e3f72ce36e
Marcos Couto <marcos_otuoc@hotmail.com>
2020-01-25 17:47:32 -0300

    Including back-end elements to thymeleaf template post.html

--- src/main/java/com/spring/codeblog/util/DummyData.java
+++ src/main/java/com/spring/codeblog/util/DummyData.java
@@ -5,6 +5,7 @@ import com.spring.codeblog.repository.CodeblogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
